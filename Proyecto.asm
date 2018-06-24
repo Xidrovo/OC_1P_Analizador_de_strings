1d62b5883b3d38196c6a4ed8b7e4161025bc7ab5
Xavier Idrovo <xidrovo@hotmail.com>
2018-06-23 23:10:33 -0400

    minor fix
    also, agregado links al readme :dancers:

--- Proyecto.asm
+++ Proyecto.asm
@@ -3,21 +3,21 @@
 .text
 
 main:
-    li $t1,0#Inicializamos nuestro contador que va a estar en $t1 con el valor de 0
-    la $t0,message#Guardamos el primer valor del texto en el registro
+    li $t1,$zero		#Inicializamos nuestro contador que va a estar en $t1 con el valor de 0
+    la $t0,message		#Guardamos el primer valor del texto en el registro
 
 loop:
-    lb   $a0,0($t0)#Cargamos un byte del registro $t0 (Nuestro texto), lo que sería la primera letra
-    beqz $a0, fin #Si encuentra que es un valor nulo, se salta a la función de fin.
-    addi $t0,$t0,1#Añade uno al registro de nuestro texto (ir al caracter siguiente)
-    addi $t1,$t1,1#Añade uno a nuestro contador que va a ser guardado en $t1
-    j     loop #Repite el loop
+    lb   $a0,0($t0)		#Cargamos un byte del registro $t0 (Nuestro texto), lo que sería la primera letra
+    beqz $a0, fin 		#Si encuentra que es un valor nulo, se salta a la función de fin.
+    addi $t0,$t0,1		#Añade uno al registro de nuestro texto (ir al caracter siguiente)
+    addi $t1,$t1,1		#Añade uno a nuestro contador que va a ser guardado en $t1
+    j     loop 			#Repite el loop
     
     
-fin:#Cuando ya se termine se ejecuta esta funcion
-    li   $v0,1#Instruccion para imprimir un Integer (nuestro contador)
-    add  $a0, $0,$t1#Añadir el valor de nuestro contador al registro que se va a imprimir
-    syscall#Ejecuta imprimir el valor del contador
+fin:					#Cuando ya se termine se ejecuta esta funcion
+    li   $v0,1			#Instruccion para imprimir un Integer (nuestro contador)
+    add  $a0, $0,$t1	#Añadir el valor de nuestro contador al registro que se va a imprimir
+    syscall				#Ejecuta imprimir el valor del contador
 
-    li   $v0,10#Instrucción para terminar el programa
-    syscall#Ejecutar terminar programa.
+    li   $v0,10			#Instrucción para terminar el programa
+    syscall				#Ejecutar terminar programa.
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # OC_1P_Analizador_de_strings
 Proyecto de primer parcial de Organización por computadores
+
+## Syscall
+http://courses.missouristate.edu/KenVollmar/Mars/Help/SyscallHelp.html
+
+## Comandos
+http://www.mrc.uidaho.edu/mrc/people/jff/digital/MIPSir.html
+
+## Pseudoinstrucciones
+https://en.wikibooks.org/wiki/MIPS_Assembly/Pseudoinstructions
+
+## Ejemplos con comparaciones al lenguaje C
+https://courses.cs.washington.edu/courses/cse378/03wi/lectures/mips-asm-examples.html
